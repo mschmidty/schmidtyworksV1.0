da59c3c0576782f80bdeaeb57d1107113f0ddc22
Michael Schmidt <mschmidty@gmail.com>
2016-03-08 23:41:27 -0700

    fixed scss browser-sync issue

--- gulpfile.js
+++ gulpfile.js
@@ -48,15 +48,19 @@ gulp.task('browser-sync', ['styles', 'jekyll-build'], function() {
         }
     });
 });
+
 // Compile Our Sass
 gulp.task('styles', function() {
     return gulp.src('src/scss/*.scss')
 	    .pipe(sass({ style: 'expanded' }))
 	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	    .pipe(gulp.dest('css'))
+	   
 	    .pipe(rename({suffix: '.min'}))
 	    .pipe(minifycss())
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
 	    .pipe(gulp.dest('css'))
+
 });
 
 gulp.task('scripts', function() {
